7209dcad0eff5935c68740166bbcbdee52817807
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-02-02 19:19:27 +0530

    feat: Add CORS support to the application and update package dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 // routes/index.js
 import express from 'express';
+import cors from 'cors';
 import translateRouter from './translator.routes.js';
 import flashCardsRouter from './flashCards.routes.js';
 import grammarExerciseRouter from './grammarExercise.routes.js';
 
 const router = express.Router();
+
+// Enable CORS for all routes with all origins
+router.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+}));
 
 router.use('/translate', translateRouter);
 router.use('/flashcards', flashCardsRouter);
